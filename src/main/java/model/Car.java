32eab31b3e862da9bb0fd80acbce4639cba5f7a9
Andrew <andreybybnov435@yandex.ru>
2026-07-22 13:57:42 +0500

    Create Car class

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,71 @@
+package model;
+
+public class Car {
+
+    private final String model;
+    private final int power;
+    private final int year;
+
+    private Car(Builder builder) {
+
+        this.model = builder.model;
+        this.power = builder.power;
+        this.year = builder.year;
+    }
+
+    public String getModel() {
+
+        return model;
+    }
+
+    public int getPower() {
+
+        return power;
+    }
+
+    public int getYear() {
+
+        return year;
+    }
+
+    public static Builder builder() {
+
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private String model;
+        private int power;
+        private int year;
+
+        public Builder model(String model) {
+
+            this.model = model;
+            return this;
+        }
+
+        public Builder power(int power) {
+
+            this.power = power;
+            return this;
+        }
+
+        public Builder year(int year) {
+
+            this.year = year;
+            return this;
+        }
+
+        public Car build() {
+
+            return new Car(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+
+        return "Car{model='" + model + "', power=" + power + ", year=" + year + "}";
+    }
+}
