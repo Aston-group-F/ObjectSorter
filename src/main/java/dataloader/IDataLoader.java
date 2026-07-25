1ad790e1ce463692b41577173a6d48390791db69
diana.mizina <diana.mizina@yandex.ru>
2026-07-25 11:48:59 +0300

    OS-4: realization of FileDataLoader

--- src/main/java/dataloader/IDataLoader.java
+++ src/main/java/dataloader/IDataLoader.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public interface IDataLoader {
     /**
+     * Loads a collection of Cars in a certain way
+     *
      * @return Collection of Cars
      */
     List<Car> load(int carsCount);
